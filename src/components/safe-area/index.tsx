225171f763f02131ee87380a4bbf0be761c5cddb
Abhishek Patel <abhipatel7420@gmail.com>
2022-12-09 15:18:03 +0530

    feat: add lottie animation on main page

--- src/components/safe-area/index.tsx
+++ src/components/safe-area/index.tsx
@@ -4,4 +4,5 @@ import styled from "styled-components/native";
 export const SafeArea = styled.SafeAreaView`
   flex: 1;
   ${StatusBar.currentHeight != null && `margin-top: ${StatusBar.currentHeight}px`};
+  background-color: ${({ theme: { colors } }) => colors.bg.primary};
 `;
--- src/features/account/screens/account/index.tsx
+++ src/features/account/screens/account/index.tsx
@@ -1,8 +1,10 @@
+import React from "react";
 import { ParamListBase, useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
+import LottieView from "lottie-react-native";
 
 import { Spacer } from "../../../../components";
-import { AccountBackground, AccountContainer, AccountCover, AuthButton, Title } from "../../components";
+import { AccountBackground, AccountContainer, AccountCover, AnimationWrapper, AuthButton, Title } from "../../components";
 
 export const AccountScreen = () => {
   const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
@@ -10,6 +12,9 @@ export const AccountScreen = () => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AnimationWrapper>
+        <LottieView key="animation" autoPlay loop resizeMode="cover" source={require("../../../../../assets/animations/watermelon.json")} />
+      </AnimationWrapper>
       <Title>Meals To Go</Title>
       <AccountContainer>
         <AuthButton icon="lock-open-outline" mode="contained" onPress={() => navigation.navigate("Login")}>
